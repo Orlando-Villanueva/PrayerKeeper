b67f7b1e19624f4372313e511fdabbeeffe1f810
Orlando Villanueva <orlandovillanueva11@gmail.com>
2025-04-01 14:44:18 +0000

    cleanup

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  server: {
-    allowedHosts: [
-      "bead384b-e6f8-4448-a158-d3cc14d235a9-00-518tr9h6oej7.worf.replit.dev",
-    ],
-  },
 });
