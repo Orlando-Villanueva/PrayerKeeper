163d6e69ffc95a504b1d8a97c3ac35c8e36edfc2
Orlando <orlandovillanueva11@gmail.com>
2025-04-19 15:18:39 -0400

    Update PrayerModal component: Modify icon display logic to differentiate between edit and default modes, enhancing user interface clarity.

--- src/components/prayer/PrayerModal.vue
+++ src/components/prayer/PrayerModal.vue
@@ -86,7 +86,10 @@
         class="w-full sm:w-auto"
       >
         <template #icon v-if="!prayerStore.loading">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg v-if="isEditMode" xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+          </svg>
+          <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1.5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
           </svg>
         </template>
