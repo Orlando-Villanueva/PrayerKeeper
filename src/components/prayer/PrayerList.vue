4b36d4edca0fd0b7dc67c3bbccbe5df6b41c06b8
Orlando <orlandovillanueva11@gmail.com>
2025-04-19 15:07:07 -0400

    Fix openEditModal In PrayerList

--- src/components/prayer/PrayerList.vue
+++ src/components/prayer/PrayerList.vue
@@ -69,7 +69,7 @@
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19a7 7 0 10-7-7v4m0 0l-4-4m4 4l4-4" />
 </svg>
             </BaseActionButton>
-            <BaseActionButton @click="openEditModFal(prayer)" variant="primary" title="Edit prayer">
+            <BaseActionButton @click="openEditModal(prayer)" variant="primary" title="Edit prayer">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                 stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
